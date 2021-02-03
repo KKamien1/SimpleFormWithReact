c334769a254d07f3f8ecce7d23970c815e414d9e
Kris <kamienieckik@gmail.com>
2021-02-03 22:50:36 +0100

    Initial setup: enzyme, node-sass, html form

--- src/tasks/form-task/FormTaskComplete/FormTaskComplete.js
+++ src/tasks/form-task/FormTaskComplete/FormTaskComplete.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
 export const FormTaskComplete = () => {
-    return (<div />);
+    return (
+        <>
+            <form>
+                <fieldset >
+                    <legend>Personal Info</legend>
+                    <label>First Name: <input type='text' required /></label>
+                    <label>Last Name: <input type='text' required /></label>
+                    <label>Birthday: <input type='date' /></label>
+                </fieldset>
+
+                <fieldset >
+                    <legend>User Management</legend>
+                    <label>User Type: <input type='text' /></label>
+                    <label>User Inactivity Date: <input type='date' /></label>
+                </fieldset>
+                <button>Save</button>
+            </form>
+        </>
+    );
 };
--- src/tasks/form-task/FormTaskComplete/FormTaskComplete.test.js
+++ src/tasks/form-task/FormTaskComplete/FormTaskComplete.test.js
@@ -1,5 +1,17 @@
+import React from 'react';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
+
+import { FormTaskComplete } from './FormTaskComplete'
+
+configure({ adapter: new Adapter() });
+
 describe('FormTaskComplete', () => {
-    test('form dummy test', () => {
-        expect(true).toBe(true);
-    });
+
+    test('should match snapshot', () => {
+        const wrapper = shallow(<FormTaskComplete />);
+        expect(wrapper.html()).toMatchSnapshot();
+    })
+
 });
